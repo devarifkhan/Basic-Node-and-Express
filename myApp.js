583c1ade8cbadcd3014c13361e3d9de98000863d
devarifkhan <arifcse209@gmail.com>
2022-08-08 21:50:24 +0600

    first commit

--- myApp.js
+++ myApp.js
@@ -1,15 +1,21 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World")
 
 
+app.get('/', function (req, res) {
+   
+    res.sendFile(__dirname + '/views/index.html');
 
+});
 
+app.use("/public", express.static(__dirname + "/views/index.html"));
 
-
-
-
-
+app.get("/json", function(req, res) {
+  res.json({"message": "Hello json"}
+  );
+});
 
 
 
